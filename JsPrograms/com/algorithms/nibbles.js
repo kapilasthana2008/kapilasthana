61bb013f38fe48e986fdcf32ba8d46b97afe552f
kapilasthana2008 <kapilasthana2008@gmail.com>
2019-09-16 19:21:17 +0530

    nibbles program was working

--- JsPrograms/com/algorithms/nibbles.js
+++ JsPrograms/com/algorithms/nibbles.js
@@ -1,21 +1,36 @@
-function tree()
-{
-    console.log("hi kapil asthana")
-}
-function one()
-{
+const util = require("./utility")
+console.log("enter number to find nibbles")
+let num =parseInt( util.data.input())
 
-    throw new Error("kapil asthana something went wrong...");
-    tree()
-}
-function two()
+
+if(isNaN(num)|| num > 128)
 {
-    try {
-        one();
-    } catch (error) {
-     
+    console.log("It may not a number or may be exceed from 128.")
+}
+else{
+    var arr = findBin(num)
+}
+
+
+//function to get binary digit
+function findBin(deci)
+{
+    var arr = []
+   
+    if(isNaN(deci))
+    {
+        console.log("your entered decimal is may be greater 128 or may not a number.")
+        return
     }
-  
+    else{
+        var bin = deci.toString(2)
+        console.log(`binary is ${bin}`)
+    
+        for(let i = bin.length-1;i>=0;i--)
+        {
+            console.log(bin[i])
+            arr.push(parseInt(bin[i]))
+        }
+        return arr
+    }
 }
-
-two();
